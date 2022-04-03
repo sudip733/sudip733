4d1f45bbfaf5493950b92b455fdcfa227239795e
sudip733 <sdpsbdi@gmail.com>
2022-04-03 08:06:39 +0545

    project initialized

---
+++ classes.py
@@ -0,0 +1,57 @@
+# class Student:
+#     name = ""
+#     gender = ""
+#     age = ""
+#     nationality = ""
+#     email = ""
+#     phone = ""
+#     course = ""
+#     username = ""
+#     password = ""
+#
+#     def __init__(self):
+#         pass
+#
+#     def setvalues(self, name, gender, dob, nationality, email, phone, course, username, password):
+#         self.name
+#         self.gender
+#         self.dob
+#         self.nationality
+#         self.email
+#         self.phone
+#         self.course
+#         self.username
+#         self.password
+#
+#     def addStudent(self, std_list):
+#         std_list.append(self.name, self.gender)
+#
+#
+#     def getStudent(self):
+#         pass
+#
+#     def updateStudent(self):
+#         pass
+#
+#     def searchStudent(self):
+#         pass
+#
+#     def deleteStudent(self):
+#         pass
+#
+#
+# name = input('Enter your name')
+# gender = input('Enter your gender')
+# dob = input('Enter your date of birth')
+# nationality = input('Enter your nationality')
+# email = input('Enter your email')
+# phone = input('Enter your mobile')
+# course = input("Enter your course")
+# username = input('Enter username')
+# password = input('Enter password')
+#
+# student_list = []
+# st = Student()
+# st.setvalues(name, gender, dob, nationality, email, phone, course, username, password)
+# st.addStudent(student_list)
+# print(student_list)
